b38f3448442caa74a689716ee77e105087968dd7
DiegoGuevaraO <dguevaraorduna@gmail.com>
2023-10-25 23:57:25 -0600

    Mejora en diseño de Portfolio y filtro de proyectos devuelve data correcta

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,7 +1,16 @@
 export const Projects = [
     {
         name:"Proyecto 1",
+        img: "/blog-app-project.png",
         description:"Esta es una prueba de descripción de proyecto",
-        keywords:"react, nextjs, tailwind"
+        keywords:"react, nextjs, tailwind",
+        href:""
+    },
+    {
+        name:"Proyecto 2",
+        img: "",
+        description:"Esta es una prueba de descripción de proyecto 2",
+        keywords:"django, python, bootstrap",
+        href:""
     }
 ];
--- pages/portfolio.jsx
+++ pages/portfolio.jsx
@@ -3,11 +3,27 @@ import Head from "next/head";
 import NavBar from "@/components/navigation/NavBar";
 import Skeleton from "@/components/Skeleton";
 import { Projects } from "@/components/Projects";
+import { FaSearch } from 'react-icons/fa';
 
 const Portfolio = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [searchInput, setSearchInput] = useState("");
     const [filteredResults, setFilteredResults] = useState([]);
+
+    const handleChange = (e) => {
+        setSearchInput(e.target.value)
+    }
+
+    const handleSearch = () => {
+        const filteredData = Projects.filter((keyword) => {
+            return Object.values(keyword)
+              .join("")
+              .toLowerCase()
+              .includes(searchInput.toLowerCase());
+        });
+        
+        setFilteredResults(filteredData);
+    }
 
     return(
         <>
@@ -15,42 +31,50 @@ const Portfolio = () => {
                 <title>Diego Guevara - Portfolio</title>
             </Head>
             <NavBar />
-            <div className="md:container md:mx-auto">
-                <div className="mb-5">
+            <div className="">
+                <div className="flex items-center border-b-2 py-2 mb-5">
                     <label htmlFor="search" className="m-auto">
                         <input 
                             type="search"
                             name="search"
                             id="search"
-                            placeholder="Search keywords, technologies..."
-                            className="bg-transparent mr-3 px-2 h-5"
+                            placeholder="Search keywords"
+                            onChange={handleChange}
+                            value={searchInput}
+                            className="bg-transparent mr-3 px-2 h-10 w-auto"
                         />
                     </label>
+                    <button onClick={handleSearch} className="right-0 top-0 mt-3 mr-4">
+                        <FaSearch className="h-4 w-4 fill-current" />
+                    </button>
                 </div>
-                <hr className="border-gray-400 mx-44 mb-5" />
-                {
-                    isLoading ?
-                    <Skeleton /> : 
-                    Projects.map((project,key) => {
-                        return(
-                            <div key={key} className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-4">
-                                <div className="bg-white rounded-lg border p-4">
-                                    <img src="https://placehold.co/300x200/d1d4ff/352cb5.png" alt="Placeholder Image" className="w-full h-48 rounded-md object-cover" />
-                                    <div className="px-1 py-4">
-                                    <div className="font-bold text-xl mb-2">{project.name}</div>
-                                    <p className="text-gray-700 text-base">
-                                        {project.description}
-                                    </p>
-                                    </div>
-                                    <div className="px-1 py-4">
-                                    <a href="#" className="text-blue-500 hover:underline">Read More</a>
-                                    </div>
-                                </div>
-                            </div>
-                        );
-                    })
-                }
-             </div>
+                <div className="flex items-center justify-center lg:h-screen">
+                    <div className="container mx-auto p-4">
+                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-4">
+                            {
+                                Projects.map((project,key) => {
+                                    return(
+                                        <div key={key} className="bg-white rounded-lg border p-4">
+                                            <img src={project.img} alt={project.name} className="w-full h-48 rounded-md object-cover" />
+                                            <div className="px-1 py-4">
+                                                <div className="font-bold text-xl mb-2">
+                                                    {project.name}
+                                                </div>
+                                                <p className="text-gray-700 text-base">
+                                                    {project.description}
+                                                </p>
+                                            </div>
+                                            <div className="px-1 py-4">
+                                                <a href={project.href} className="text-blue-700 hover:underline">Read More</a>
+                                            </div>
+                                        </div>
+                                    );
+                                })
+                            }
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
